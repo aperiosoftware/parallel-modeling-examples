8ad82fa7c1a9e3dc60c14949f5dca1d9d972a191
Thomas Robitaille <thomas.robitaille@gmail.com>
2024-07-22 13:11:48 +0100

    Updated L1448 script

--- l1448-spectral-cube/fit_no_world.py
+++ l1448-spectral-cube/fit_no_world.py
@@ -1,15 +1,10 @@
-# Simple spectral cube, no world coordinates
-
 from astropy.io import fits
-from astropy.wcs import WCS
 
-from tqdm.dask import TqdmCallback
 import matplotlib.pyplot as plt
 
-import numpy as np
 from astropy.modeling.models import Gaussian1D
 from astropy.modeling.fitting import LMLSQFitter
-from astropy.modeling.fitting_parallel import parallel_fit_model_nd
+from astropy.modeling.fitting_parallel import parallel_fit_dask
 
 if __name__ == "__main__":
 
@@ -18,22 +13,18 @@ if __name__ == "__main__":
     g_init = Gaussian1D(mean=25, stddev=10, amplitude=1)
     g_init.mean.bounds = (0, 53)
 
-    with TqdmCallback(desc="fitting"):
-
-        g_fit = parallel_fit_model_nd(
-            model=g_init,
-            fitter=LMLSQFitter(),
-            data=data,
-            fitting_axes=0,
-            diagnostics="failed+warn",
-            diagnostics_path="diag",
-        )
+    g_fit = parallel_fit_dask(
+        model=g_init,
+        fitter=LMLSQFitter(),
+        data=data,
+        fitting_axes=0,
+    )
 
     fig = plt.figure(figsize=(10, 5))
     ax = fig.add_subplot(1, 3, 1)
-    ax.imshow(g_fit.amplitude.value, vmin=0, vmax=5)
+    ax.imshow(g_fit.amplitude.value, vmin=0, vmax=5, origin="lower")
     ax = fig.add_subplot(1, 3, 2)
-    ax.imshow(g_fit.mean.value, vmin=0, vmax=50)
+    ax.imshow(g_fit.mean.value, vmin=0, vmax=50, origin="lower")
     ax = fig.add_subplot(1, 3, 3)
-    ax.imshow(g_fit.stddev.value, vmin=0, vmax=20)
-    fig.savefig("results_no_world.png")
+    ax.imshow(g_fit.stddev.value, vmin=0, vmax=20, origin="lower")
+    fig.savefig("results_no_world.png", bbox_inches="tight")
